b5614f77f8a616e169dc8bc5b769b792c908fdbd
ByCheno <jcoronelcamacho@gmail.com>
2024-03-18 21:00:32 +0100

    Navbar, isActive, styles

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,10 +3,15 @@ import { DriversInfo } from "../r-drivers/pages/DriversInfo"
 import { DriversStandings } from "../r-drivers/pages/DriversStandings"
 import { TeamsInfo } from "../r-teams/pages/TeamsInfo"
 import { TeamsStandings } from "../r-teams/pages/TeamsStandings"
+import { Navbar } from "../ui"
+import { Login } from "../auth/pages/Login"
 
 export const AppRouter = () => {
     return (
         <>
+
+            <Navbar />
+
             <Routes>
                 {/* Ruta por defecto, POR HACER: ESTABLECER UNA RUTA DE INICIO DE LA WEB */}
                 <Route path="/*" element={<Navigate to={'/driversInfo'} />} />
@@ -18,6 +23,9 @@ export const AppRouter = () => {
                 {/* Rutas para las escuderias */}
                 <Route path="teamsInfo" element={<TeamsInfo />} />
                 <Route path="teamsStandings" element={<TeamsStandings />} />
+
+                {/* Ruta para el login */}
+                <Route path="login" element={<Login />} />
             </Routes>
         </>
     )
---
+++ src/ui/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import { Link, NavLink } from 'react-router-dom';
+
+
+export const Navbar = () => {
+    return (
+
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark rounded-3">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/"> Esquizo F1 League </Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                            <NavLink className={ ({isActive}) => `nav-link ${isActive ? 'active' : ''}` } to="/driversInfo">
+                                Drivers Info
+                            </NavLink>
+
+                            <NavLink className={ ({isActive}) => `nav-link ${isActive ? 'active' : ''}` } to="/driversStandings">
+                                Drivers Standings
+                            </NavLink>
+
+                            <NavLink className={ ({isActive}) => `nav-link ${isActive ? 'active' : ''}` } to="/teamsInfo">
+                                Teams Info
+                            </NavLink>
+
+                            <NavLink className={ ({isActive}) => `nav-link ${isActive ? 'active' : ''}` } to="/teamsStandings">
+                                Teams Standings
+                            </NavLink>
+                    </ul>
+                </div>
+                <div className="navbar-collapse">
+                    <ul className="navbar-nav ml-auto">
+                        <NavLink to="/login" className={ ({isActive}) => `nav-link ${isActive ? 'active' : ''}` }>
+                            Login
+                        </NavLink>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
